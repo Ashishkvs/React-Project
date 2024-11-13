9915481e28e1c96446aba4591807148319f24951
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-11-13 18:35:44 +0530

    Update AppSetting.js

--- src/page/AppSetting.js
+++ src/page/AppSetting.js
@@ -8,15 +8,15 @@ export default AppSetting;
 // simple to use 
 // declare in  file an import and use
 
-/// this is a goog fit if our use case is only read data from global level and share across any component
+/// this is a good fit if our use case is only read data from global level and share across any component
 
 // problem use case
 
-// 1. if nay component manipulate its value ( AppSetting.nam = "Ashishs") it will be reflecting in that context only
+// 1. if any component manipulate its value ( AppSetting.name = "Ashish") it will be reflecting in that context only
 // due to that data incosistency arises
-// It wont update othe using component ,they have still stale (old config data ) even after updating it from any component
+// It won't update othe using component ,they have still stale (old config data ) even after updating it from any component
 
 
-// this is a normla javascript object
-//it doesnt have state management capability
+// this is a normal javascript object
+//it doesn't have state management capability
 
